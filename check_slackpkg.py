750211a29f43fa278a225f651dc74372c44b5140
nihlaeth <info@nihlaeth.nl>
2016-01-20 10:15:06 +0100

    the actual thing

---
+++ check_slackpkg.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""Nagios module for monitoring available updates via slackpkg."""
+
+import subprocess
+import sys
+
+# pylint: disable=invalid-name
+
+# run check-updates to poll mirror for changes
+result = []
+try:
+    result = subprocess.check_output("slackpkg check-updates", shell=True).split("\n")
+except (OSError, subprocess.CalledProcessError) as error:
+    print "Failed to check for updates: %s" % error
+    sys.exit(3)
+
+updates = "idk"
+for line in result:
+    if "good news" in line:
+        updates = "no"
+    elif "News on" in line:
+        updates = "yes"
+if updates == "idk":
+    print "Error parsing slackpkg check-updates status"
+    sys.exit(3)
+elif updates == "yes":
+    # fetch updated package list
+    try:
+        subprocess.check_call("slackpkg update")
+    except (OSError, subprocess.CalledProcessError) as error:
+        print "Failed to update package list: %s" % error
+        sys.exit(3)
+
+# Now the packages list is up to date, check if we need to upgrade anything
+result = []
+try:
+    result = subprocess.check_output([
+        "slackpkg",
+        "-dialog=off",
+        "-default_answer=n",
+        "-batch=on",
+        "upgrade-all"]).split("\n")
+except (OSError, subprocess.CalledProcessError) as error:
+    print "Failed to check for upgrades: %s" % error
+    sys.exit(3)
+
+packages = []
+for line in result:
+    if ".txz" in line:
+        packages.append(line.strip())
+
+if len(packages) == 0:
+    print "OK: everything up-to-date"
+    sys.exit(0)
+else:
+    print "Updates available: " + " ".join(packages)
+    sys.exit(2)
